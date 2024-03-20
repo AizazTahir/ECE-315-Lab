463926732c92a98e638ca4ca732cf51adbe84366
Nnamdi Ajoku <ajayd@MacBook-Air-4.local>
2024-03-20 16:42:42 -0600

    Add OLED_PutPixel function to OLEDGraphics.h

--- Lab_3/lab_3_hw/lab_3_hw.sdk/part_2/src/OLEDControllerCustom.h
+++ Lab_3/lab_3_hw/lab_3_hw.sdk/part_2/src/OLEDControllerCustom.h
@@ -6,5 +6,6 @@
 // Function prototypes
 void OLED_DrawLineTo(PmodOLED *InstancePtr, int xco, int yco);
 void OLED_RectangleTo(PmodOLED *InstancePtr, int xco, int yco);
+void OLED_PutPixel(PmodOLED *InstancePtr, int x, int y);
 
 #endif // OLEDGRAPHICS_H
